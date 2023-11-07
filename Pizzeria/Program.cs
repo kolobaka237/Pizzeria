7ae607380e93f119636850823b3fec3dbcba9507
kolobaka237 <polyuga.dima@gmail.com>
2023-11-07 22:44:42 +0200

    Добавил акцию "каждая 5-я пицца бесплатно"

--- Pizzeria/Program.cs
+++ Pizzeria/Program.cs
@@ -1,109 +1,146 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hi, it's Freddy Fazbear's Pizzeria.");
-        Console.WriteLine("Today we have a special offer: a 20% discount for orders over $50. Every 5th pizza is free. For drinks priced at $3 or more, there is a 15% discount when you buy more than 3 items.");
-        Console.WriteLine("Choose a pizza: \nCarbonara - 1\t 15$ \nMeat - 2\t 25$ \nHawaiian - 3\t 20$ \nCheese - 4\t 23$ ");
-        Pizza pizza = (Pizza)Convert.ToInt16(Console.ReadLine());
-        Console.WriteLine("How many pizzas to cook?");
-        int pizzaQuantity = Convert.ToInt16(Console.ReadLine());
-        Console.WriteLine("Choose a drink: \nCola - 1\t 3$ \nFanta - 2\t 2$ \nSprite - 3\t 2$");
-        Drink drink = (Drink)Convert.ToInt16(Console.ReadLine());
-        Console.WriteLine("how many drinks do you want?");
-        int drinkQuantity = Convert.ToInt16(Console.ReadLine());
-        double totalCost = 0;
-        switch (pizza)
+        try
         {
-            case Pizza.Carbonara:
+            Console.WriteLine("Hi, it's Freddy Fazbear's Pizzeria.");
+            Console.WriteLine("Today we have a special offer: a 20% discount for orders over $50. Every 5th pizza is free. For drinks priced at $3 or more, there is a 15% discount when you buy more than 3 items.");
+            Console.WriteLine("Choose a pizza: \nCarbonara - 1\t 15$ \nMeat - 2\t 25$ \nHawaiian - 3\t 20$ \nCheese - 4\t 23$ ");
+            Pizza pizza = (Pizza)Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("How many pizzas to cook?");
+            int pizzaQuantity = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Choose a drink: \nCola - 1\t 3$ \nFanta - 2\t 2$ \nSprite - 3\t 2$");
+            Drink drink = (Drink)Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("how many drinks do you want?");
+            int drinkQuantity = Convert.ToInt16(Console.ReadLine());
+            double totalCost = 0;
+            int freePizza = 0;
+            switch (pizza)
+            {
+                case Pizza.Carbonara:
 
-                int pizzaPrice = 15;
-                double result = pizzaQuantity * pizzaPrice;
-                if (result > 50)
-                {
-                    double sale = result / 100 * 20;
-                    result = result - sale;
-                }
-                totalCost += result;
-                Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
-                break;
-            case Pizza.Meat:
-                pizzaPrice = 25;
-                result = pizzaQuantity * pizzaPrice;
-                if (result > 50)
-                {
-                    double sale = result / 100 * 20;
-                    result = result - sale;
-                }
-                Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
-                break;
-            case Pizza.Hawaiian:
-                pizzaPrice = 20;
-                result = pizzaQuantity * pizzaPrice;
-                if (result > 50)
-                {
-                    double sale = result / 100 * 20;
-                    result = result - sale;
-                }
-                Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
-                break;
-            case Pizza.Cheese:
-                pizzaPrice = 23;
-                result = pizzaQuantity * pizzaPrice;
-                if (result > 50)
-                {
-                    double sale = result / 100 * 20;
-                    result = result - sale;
-                }
-                Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
-                break;
+                    int pizzaPrice = 15;
+                    double result = pizzaQuantity * pizzaPrice;
+                    if (result > 50)
+                    {
+                        double sale = result / 100 * 20;
+                        result = result - sale;
+                    }
+                    if (pizzaQuantity % 5 == 0)
+                    {
+                        freePizza = pizzaQuantity / 5;
+                        totalCost -= 15 * freePizza;
+                    }
+                    totalCost += result;
+                    Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
+                    break;
+                case Pizza.Meat:
+                    pizzaPrice = 25;
+                    result = pizzaQuantity * pizzaPrice;
+                    if (result > 50)
+                    {
+                        double sale = result / 100 * 20;
+                        result = result - sale;
+                    }
+                    if (pizzaQuantity % 5 == 0)
+                    {
+                        freePizza = pizzaQuantity / 5;
+                        totalCost -= 25 * freePizza;
+                    }
+                    totalCost += result;
+                    Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
+                    break;
+                case Pizza.Hawaiian:
+                    pizzaPrice = 20;
+                    result = pizzaQuantity * pizzaPrice;
+                    if (result > 50)
+                    {
+                        double sale = result / 100 * 20;
+                        result = result - sale;
+                    }
+                    if (pizzaQuantity % 5 == 0)
+                    {
+                        freePizza = pizzaQuantity / 5;
+                        totalCost -= 20 * freePizza;
+                    }
+                    totalCost += result;
+                    Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
+                    break;
+                case Pizza.Cheese:
+                    pizzaPrice = 23;
+                    result = pizzaQuantity * pizzaPrice;
+                    if (result > 50)
+                    {
+                        double sale = result / 100 * 20;
+                        result = result - sale;
+                    }
+                    if (pizzaQuantity % 5 == 0)
+                    {
+                        freePizza = pizzaQuantity / 5;
+                        totalCost -= 23 * freePizza;
+                    }
+                    totalCost += result;
+                    Console.WriteLine($"Your order:\n{pizza}\t {pizzaQuantity} шт\t {result}$");
+                    break;
 
 
+            }
+            switch (drink)
+            {
+                case Drink.Cola:
+                    int drinkPrice = 3;
+                    double result = drinkQuantity * drinkPrice;
+                    if (drinkQuantity > 3)
+                    {
+                        double sale = result / 100 * 15;
+                        result = result - sale;
+                    }
+                    totalCost += result;
+                    Console.WriteLine($"{drink} \t\t {drinkQuantity} шт\t {result}$");
+                    break;
+                case Drink.Fanta:
+                    int fantaPrice = 2;
+                    result = drinkQuantity * fantaPrice;
+                    totalCost += result;
+                    Console.WriteLine($"{drink} \t\t {drinkQuantity} шт\t {result}$");
+                    break;
+                case Drink.Sprite:
+                    int spritePrice = 2;
+                    result = drinkQuantity * spritePrice;
+                    totalCost += result;
+                    Console.WriteLine($"{drink} \t\t {drinkQuantity} шт\t {result}$");
+                    break;
+
+            }
+
+            Console.WriteLine($"You take {freePizza} free pizza");
+            Console.WriteLine($"Total cost: {totalCost}$");
         }
-        switch (drink)
+        catch (Exception ex)
         {
-            case Drink.Cola:
-                int drinkPrice = 3;
-                double result = drinkQuantity * drinkPrice;
-                if (drinkQuantity > 3)
-                {
-                    double sale = result / 100 * 15;
-                    result = result - sale;
-                }
-                Console.WriteLine($"{drink} \t\t {drinkQuantity} шт\t {result}$");
-                break;
-            case Drink.Fanta:
-                int fantaPrice = 2;
-                result = drinkQuantity * fantaPrice;
-                Console.WriteLine($"{drink} \t\t {drinkQuantity} шт\t {result}$");
-                break;
-            case Drink.Sprite:
-                int spritePrice = 2;
-                result = drinkQuantity * spritePrice;
-                Console.WriteLine($"{drink} \t\t {drinkQuantity} шт\t {result}$");
-                break;
-
+            Console.WriteLine(ex.Message);
         }
-        Console.WriteLine($"Total cost: {totalCost}$");
     }
 
+    enum Drink
+    {
+        Cola = 1,
+        Fanta,
+        Sprite
 
-
-enum Drink
-{
-    Cola = 1,
-    Fanta,
-    Sprite
-
-}
-enum Pizza
-{
-    Carbonara = 1,
-    Meat,
-    Hawaiian,
-    Cheese
-}
+    }
+    enum Pizza
+    {
+        Carbonara = 1,
+        Meat,
+        Hawaiian,
+        Cheese
+    }
 }
     
 
